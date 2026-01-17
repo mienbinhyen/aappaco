75ef61efbb7090495671b9255c1fc52e98bc6607
mienbinhyen <tgthiennhan@gmail.com>
2026-01-17 10:32:23 +0700

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,19 @@
+
+/**
+ * @file index.tsx
+ * Entry point for the Paco Audio React application.
+ */
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+const container = document.getElementById('root');
+if (container) {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
